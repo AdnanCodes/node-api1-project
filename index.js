c3c9f4faef0058c6475abd71d186905274a1bd9b
Adnan <adnan.chowdhury@outlook.com>
2019-09-02 15:53:36 -0500

    Project Setup, Express setup on Port 3000, packages added

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+const port = 3000;
+
+server.listen(port, () => console.log(`API listening on port ${port}`));
