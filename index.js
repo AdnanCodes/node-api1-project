ae759d2e9e83c46c74d7e96adaaaab180a968303
Adnan <adnan.chowdhury@outlook.com>
2019-09-02 16:24:43 -0500

    Built GET Request

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // implement your API here
 const express = require("express");
 
+const DataBase = require("./data/db");
 const server = express();
 
-const port = 3000;
+server.use(express.json());
 
+//GET request
+
+server.get("/api/users", (req, res) => {
+  DataBase.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Cannot access users data"
+      });
+    });
+});
+
+const port = 3000;
 server.listen(port, () => console.log(`API listening on port ${port}`));
