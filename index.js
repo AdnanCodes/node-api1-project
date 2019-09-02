3758ef71f421d289bdf72c92c50debf571f95b9b
Adnan <adnan.chowdhury@outlook.com>
2019-09-02 16:24:58 -0500

    Get users by ID

--- index.js
+++ index.js
@@ -20,5 +20,22 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+//GET Users by id
+
+server.get("/api/users/:id", (req, res) => {
+  DataBase.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: `The user doesn't exist with given ID` });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: `Server had issue sending the info` });
+    });
+});
 const port = 3000;
 server.listen(port, () => console.log(`API listening on port ${port}`));
